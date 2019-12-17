ea24c4d09b6b36ce97d02a1054540a813d8a2022
YEETER1234321 <51126218+YEETER1234321@users.noreply.github.com>
2019-12-17 15:26:56 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(400, 400, WEBGL);
+}
+function scene1() {
+	function draw() {
+		background(125, 125, 125);
+		fill(125, 125, 125);
+		rect(150, 100, 100, 50);
+		rect(150, 175, 100, 50);
+	}
+}
